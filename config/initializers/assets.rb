d930ad1f803a084a6c2632afd6639fc73f96bafe
joshparkerj <superfakek@gmail.com>
2018-07-06 03:22:27 -0600

    make carousel work a bit better

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,10 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( slick_carousel.js 
+slick.min.js jquery-3.3.1.min.js slick-theme.css slick-theme.less 
+slick theme.scss slick.css slick.less slick.css slick.eot slick.svg 
+slick.ttf slick.woff ajax-loader.gif )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
